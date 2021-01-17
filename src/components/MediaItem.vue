349f2c98ec5cee5ad7d5999b289c55e98f15b18a
5onderling <lenny.anders.98@gmail.com>
2021-01-17 23:18:45 +0100

    Create and utilize MediaItem components

---
+++ src/components/MediaItem.vue
@@ -0,0 +1,52 @@
+<script setup>
+  import ImdbLink from '@components/ImdbLink.vue';
+  import VimeoLink from '@components/VimeoLink.vue';
+  import YoutubeLink from '@components/YoutubeLink.vue';
+  import WebLink from '@components/WebLink.vue';
+  import { defineProps } from 'vue';
+
+  defineProps({
+    title: {
+      type: String,
+      required: true,
+    },
+    imdbId: Number,
+    youtubeId: String,
+    vimeoId: Number,
+    homepage: String,
+  });
+</script>
+
+<template>
+  <li class="media-item">
+    <div class="media-item__top">
+      <h2>{{ title }}</h2>
+      <ImdbLink v-if="imdbId" :id="imdbId" />
+      <YoutubeLink v-if="youtubeId" :id="youtubeId" />
+      <VimeoLink v-if="vimeoId" :id="vimeoId" />
+      <WebLink v-if="homepage" :url="homepage" />
+    </div>
+    <slot />
+  </li>
+</template>
+
+<style lang="scss" scoped>
+  .media-item {
+    display: grid;
+    gap: 0.5rem;
+    padding: 0.5rem 1rem;
+    background-color: #191919;
+    border-radius: 0.25rem;
+    color: #ddd;
+
+    &__top {
+      display: flex;
+      gap: 0.25rem;
+      color: #fafafa;
+
+      > h2 {
+        flex: 1;
+      }
+    }
+  }
+</style>
--- src/views/Shorts.vue
+++ src/views/Shorts.vue
@@ -1,8 +1,5 @@
 <script setup>
-  import ImdbLink from '@components/ImdbLink.vue';
-  import VimeoLink from '@components/VimeoLink.vue';
-  import YoutubeLink from '@components/YoutubeLink.vue';
-  import WebLink from '@components/WebLink.vue';
+  import MediaItem from '@components/MediaItem.vue';
 
   const shorts = Object.entries(
     import.meta.globEager('../data/shorts/*.json'),
@@ -13,60 +10,29 @@
 </script>
 
 <template>
-  <div class="filters">filter yay</div>
   <ul class="media-list">
-    <li v-for="short in shorts" :key="short.id" class="media-item">
-      <div class="media-item__top">
-        <h2>{{ short.title }}</h2>
-        <ImdbLink v-if="short.imdbId" :id="short.imdbId" />
-        <YoutubeLink v-if="short.youtubeId" :id="short.youtubeId" />
-        <VimeoLink v-if="short.vimeoId" :id="short.vimeoId" />
-        <WebLink v-if="short.homepage" :url="short.homepage" />
-      </div>
+    <MediaItem
+      v-for="short in shorts"
+      :key="short.id"
+      :title="short.title"
+      :imdbId="short.imdbId"
+      :youtubeId="short.youtubeId"
+      :vimeoId="short.vimeoId"
+      :homepage="short.homepage"
+    >
       {{ short.releaseYear }} | {{ short.duration }} min | {{ toReadableList(short.directors) }} |
       {{ toReadableList(short.styles) }}
-      <template v-if="short.franchises?.length">
-        | {{ toReadableList(short.franchises) }}
-      </template>
-    </li>
+      {{ short.franchises?.length ? ` | ${toReadableList(short.franchises)}` : '' }}
+    </MediaItem>
   </ul>
 </template>
 
 <style lang="scss" scoped>
-  .filters {
-    width: 100%;
-    height: 3rem;
-    margin: 1rem 0;
-    background-color: #888;
-  }
   .media-list {
     list-style: none;
     margin: 0;
     padding-left: 0;
     display: grid;
     gap: 0.5rem;
-  }
-
-  .media-item {
-    display: grid;
-    gap: 0.5rem;
-    padding: 0.5rem 1rem;
-    background-color: #191919;
-    border-radius: 0.25rem;
-    color: #ddd;
-
-    &__top {
-      display: flex;
-      gap: 0.25rem;
-      color: #fafafa;
-
-      > h2 {
-        flex: 1;
-      }
-    }
-  }
-
-  h2 {
-    margin: 0;
   }
 </style>
