8e76fbae1a76d52ffcbdfddfd30858316078c717
5onderling <lenny.anders.98@gmail.com>
2021-01-24 20:00:57 +0100

    use a better akross link

--- src/components/AkrossLink.vue
+++ src/components/AkrossLink.vue
@@ -11,7 +11,7 @@
 </script>
 
 <template>
-  <IconLink :href="`https://akross.ru/index.cgi?act=video;id=${id};l=e`">
+  <IconLink :href="`https://akross.ru/index.cgi?video=${id};l=e`">
     <image href="../assets/akrossIcon.ico" x="2" y="2" height="20" width="20" />
   </IconLink>
 </template>
