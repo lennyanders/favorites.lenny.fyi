28b915c622878836254d8e94ea433a624cbd567c
5onderling <lenny.anders.98@gmail.com>
2021-02-16 00:18:57 +0100

    add a few movies

--- src/film/index.11ty.js
+++ src/film/index.11ty.js
@@ -1,4 +1,6 @@
-import { html } from '~utils';
+import { css, html, toReadableList } from '~utils';
+import mediaList from '~components/mediaList';
+import mediaItem from '~components/mediaItem';
 
 export const data = {
   layout: 'base',
@@ -8,6 +10,25 @@ export const data = {
   order: 3,
 };
 
-export const render = ({}) => {
-  return html``;
-};
+const originClass = css`
+  text-transform: uppercase;
+  opacity: 0.75;
+  font-weight: 400;
+`;
+
+export const render = ({ movies }) =>
+  mediaList(
+    Object.values(movies).map((movie) => {
+      movie.title = html`${movie.title}${movie.originalTitle &&
+      ` (${movie.originalTitle}${
+        movie.origin && html` <small class="${originClass}">${movie.origin}</small>`
+      })`}`;
+
+      return mediaItem(
+        movie,
+        html`${movie.releaseYear} | ${movie.duration} min | ${toReadableList(movie.directors)} |
+        ${toReadableList(movie.styles)}${movie.franchises &&
+        ` | ${toReadableList(movie.franchises)}`}`,
+      );
+    }),
+  );
