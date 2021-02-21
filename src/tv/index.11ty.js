db9d6237fbf835914917bbe87204202a201264ba
5onderling <lenny.anders.98@gmail.com>
2021-02-21 20:39:30 +0100

    add series

--- src/tv/index.11ty.js
+++ src/tv/index.11ty.js
@@ -1,4 +1,6 @@
-import { html } from '~utils';
+import { css, html, toReadableList } from '~utils';
+import mediaList from '~components/mediaList';
+import mediaItem from '~components/mediaItem';
 
 export const data = {
   layout: 'base',
@@ -8,6 +10,36 @@ export const data = {
   order: 4,
 };
 
-export const render = ({}) => {
-  return html``;
-};
+const originClass = css`
+  text-transform: uppercase;
+  opacity: 0.75;
+  font-weight: 400;
+`;
+
+export const render = ({ series }) =>
+  mediaList(
+    Object.values(series)
+      .sort((a, b) => {
+        const textA = a.title.toUpperCase();
+        const textB = b.title.toUpperCase();
+        return textA < textB ? -1 : textA > textB ? 1 : 0;
+      })
+      .map((series) => {
+        series.title = html`${series.title}${series.originalTitle &&
+        ` (${series.originalTitle}${
+          series.origin && html` <small class="${originClass}">${series.origin}</small>`
+        })`}`;
+
+        return mediaItem(
+          series,
+          html`${series.releaseStart}${series.releaseEnd
+            ? series.releaseEnd !== series.releaseStart && ` - ${series.releaseEnd}`
+            : ' (unfinished)'}${series.canceled && ' (canceled)'}
+          | ${series.averageEpisodeDuration} min | ${series.episodeCount} episodes across
+          ${series.seasonCount} season${series.seasonCount > 1 && 's'} |
+          ${toReadableList(series.creators)} |
+          ${toReadableList(series.styles)}${series.franchises &&
+          ` | ${toReadableList(series.franchises)}`}`,
+        );
+      }),
+  );
